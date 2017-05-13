c66e404f836974f0aaaadf512312b1fb0871116b
Rob Catlin <rob.catlin@icloud.com>
2017-05-13 22:41:34 +0100

    improved mobile layout, general layout, fontawesome map icon

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 $(function(){
-    
+
+  //fixes bug that stops togglable nav from toggling back up at xs size
+  $(document).ready(function () {
+    $(".navbar-nav li a").click(function(event) {
+      $(".navbar-collapse").collapse('hide');
+    });
+  });
+
+
 });
